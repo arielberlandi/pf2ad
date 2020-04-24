542dec0813f04d84225a15f7728bd920607862fb
arielberlandi <52072360+arielberlandi@users.noreply.github.com>
2020-04-24 10:29:14 -0300

    Update pf2adED.sh

--- pf2adED.sh
+++ pf2adED.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-VERSION='20200423001' # Welcome to Portugal
+VERSION='20180302001' # Welcome to Portugal
 
 if [ -f "/etc/samba.patch.version" ]; then
 	if [ "$(cat /etc/samba.patch.version)" = "$VERSION" ]; then
@@ -11,19 +11,45 @@ fi
 
 # Verifica versao pfSense
 if [ "$(cat /etc/version)" != "2.4.5-RELEASE" ]; then
-	echo "ERROR: You need the pfSense version 2.4.4 to apply this script"
+	echo "ERROR: You need the pfSense version 2.4.3 to apply this script"
 	exit 2
 fi
 
 ASSUME_ALWAYS_YES=YES
 export ASSUME_ALWAYS_YES
 
+/usr/sbin/pkg bootstrap
+/usr/sbin/pkg update
+
+# Lock packages necessary
+/usr/sbin/pkg lock pkg
+/usr/sbin/pkg lock pfSense-2.4.5
+
+mkdir -p /usr/local/etc/pkg/repos
+
+cat <<EOF > /usr/local/etc/pkg/repos/pf2ad.conf
+pf2ad: {
+    url: "https://github.com/pf2ad/packages/raw/11.1",
+    mirror_type: "https",
+    enabled: yes
+}
+EOF
+
+/usr/sbin/pkg update -r pf2ad
+/usr/sbin/pkg install -r pf2ad net/samba44 2> /dev/null
+
+/usr/sbin/pkg unlock pkg
+/usr/sbin/pkg unlock pfSense-2.4.5
+
+rm -rf /usr/local/etc/pkg/repos/pf2ad.conf
+/usr/sbin/pkg update
+
 mkdir -p /var/db/samba4/winbindd_privileged
 chown -R :proxy /var/db/samba4/winbindd_privileged
 chmod -R 0750 /var/db/samba4/winbindd_privileged
 
-fetch -o /usr/local/pkg -q https://openjdtec.com.br/pfsense/2.4.4/samba/samba.inc
-fetch -o /usr/local/pkg -q https://openjdtec.com.br/pfsense/2.4.4/samba/samba.xml
+fetch -o /usr/local/pkg -q https://raw.githubusercontent.com/pf2ad/pf2ad/2.4.3-SAMBA4/samba.inc
+fetch -o /usr/local/pkg -q https://raw.githubusercontent.com/pf2ad/pf2ad/2.4.3-SAMBA4/samba.xml
 
 /usr/local/sbin/pfSsh.php <<EOF
 \$samba = false;
@@ -65,7 +91,7 @@ if [ ! "$(/usr/sbin/pkg info | grep pfSense-pkg-squid)" ]; then
 	/usr/sbin/pkg install -r pfSense pfSense-pkg-squid
 fi
 cd /usr/local/pkg
-fetch -o - -q https://openjdtec.com.br/pfsense/2.4.4/samba/squid_winbind_auth.patch | patch -b -p0 -f
+fetch -o - -q https://raw.githubusercontent.com/pf2ad/pf2ad/2.4.3-SAMBA4/squid_winbind_auth.patch | patch -b -p0 -f
 
 if [ ! -f "/usr/local/etc/smb4.conf" ]; then
 	touch /usr/local/etc/smb4.conf
